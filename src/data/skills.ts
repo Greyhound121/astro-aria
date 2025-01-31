2458185004c46c060cd43a639cc4d5652d144018
Prerna Dwivedi <dwivediprerna98@gmail.com>
2025-01-30 17:52:22 -0800

    deepseek-Scroller

---
+++ src/data/skills.ts
@@ -0,0 +1,26 @@
+export const skills = [
+  {
+    name: "HTML",
+    icon: "bg-[url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg')]"
+  },
+  {
+    name: "CSS",
+    icon: "bg-[url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg')]"
+  },
+  {
+    name: "JavaScript",
+    icon: "bg-[url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg')]"
+  },
+  {
+    name: "TypeScript",
+    icon: "bg-[url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg')]"
+  },
+  {
+    name: "React",
+    icon: "bg-[url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg')]"
+  },
+  {
+    name: "Node.js",
+    icon: "bg-[url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg')]"
+  }
+];
