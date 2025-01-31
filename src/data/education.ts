27e499529ab44fd3b7c84a3b56000660ff30fceb
Prerna Dwivedi <dwivediprerna98@gmail.com>
2025-01-30 19:58:28 -0800

    self fixed education

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,15 +1,17 @@
 export const education = [
   {
-    logo: "bg-[url('https://www.tru.ca/_layouts/15/TRU.Internet/images/favicon.svg')]",
+    logo: "bg-[url('/assets/images/tru.png')]",
     dates: "Dec 2021",
     degree: "Graduate Diploma in Business Administration",
     university: "Thompson Rivers University",
     gpa: "3.72/4"
   },
   {
-    logo: "bg-[url('https://christuniversity.in/images/logo.svg')]",
+    logo: "bg-[url('/assets/images/christ-university-logo.png')]",
     dates: "April 2017",
     degree: "Bachelor in Computer Application",
-    university: "Christ University"
+    university: "Christ University",
+    gpa: "3.3/4"
   }
+] as const;
 ] as const;
