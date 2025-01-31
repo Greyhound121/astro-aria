89d6768d4f8326540b4269929b41f14fa7ff1ca8
Prerna Dwivedi <dwivediprerna98@gmail.com>
2025-01-30 19:32:08 -0800

    created education

---
+++ src/data/education.ts
@@ -0,0 +1,15 @@
+export const education = [
+  {
+    logo: "/assets/images/education/tru.ico",
+    dates: "Dec 2021",
+    degree: "Graduate Diploma in Business Administration",
+    university: "Thompson Rivers University",
+    gpa: "3.72/4"
+  },
+  {
+    logo: "/assets/images/education/christ.ico",
+    dates: "April 2017",
+    degree: "Bachelor in Computer Application",
+    university: "Christ University"
+  }
+] as const;
