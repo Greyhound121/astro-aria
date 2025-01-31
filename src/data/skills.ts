65e7a95400a22f099c357089ef1ea6e92ce8854d
Prerna Dwivedi <dwivediprerna98@gmail.com>
2025-01-30 18:03:12 -0800

    addedskills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -22,5 +22,17 @@ export const skills = [
   {
     name: "Node.js",
     icon: "bg-[url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg')]"
+  },
+  {
+    name: "Git",
+    icon: "bg-[url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg')]"
+  },
+  {
+    name: "MySQL",
+    icon: "bg-[url('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg')]"
+  },
+  {
+    name: "Google Analytics",
+    icon: "bg-[url('https://www.vectorlogo.zone/logos/google_analytics/google_analytics-icon.svg')]"
   }
 ];
